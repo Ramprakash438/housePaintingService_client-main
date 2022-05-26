9177840da377e024b9729d29462c1bdd83051055
Ramprakash438 <you@example.com>
2022-05-26 20:41:04 +0530

    changed url

--- src/utils/API_URL.js
+++ src/utils/API_URL.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // backend url
 const API_URL = axios.create({
-    baseURL: "http://localhost:1003"
+    baseURL: "https://housepainting-service.herokuapp.com"
 })
 
 export default API_URL;
